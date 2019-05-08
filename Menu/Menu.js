424af702c5969cd26e68adfc8d2ac1aa326b4078
sam-malanchuk <malanchuksam@gmail.com>
2019-05-07 20:08:59 -0400

    completed Stretch animation #1 in menu.js

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,7 +1,12 @@
 
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence. 
-  menu.classList.toggle('menu--open');
+  // menu.classList.toggle('menu--open');
+  if(menu.style.left === "0px") {
+    $(".menu").animate({left: '-350px'});;
+  } else {
+    $(".menu").animate({left: '0px'});;
+  }
 }
 
 // Start Here: Create a reference to the ".menu" class
@@ -11,4 +16,10 @@ const menuButton = document.querySelector('.menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu
 menuButton.addEventListener('click', function() {
   toggleMenu();
+});
+
+document.addEventListener('click', function() {
+  if(menu.style.left === "0px") {
+    toggleMenu();
+  }
 });
