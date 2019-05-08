7edd89d735b2d188126b43ad5286c27b04f11c40
sam-malanchuk <malanchuksam@gmail.com>
2019-05-07 20:44:51 -0400

    completed Stretch delete article task in article.js

--- Article/Article.js
+++ Article/Article.js
@@ -6,10 +6,14 @@ class Article {
     this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
     this.expandButton = this.domElement.querySelector('.expandButton');
+    // create a reference to the ".close" class. 
+    this.closeButton = this.domElement.querySelector('.close');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.textContent = "Click to Expand";
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener('click', () => this.expandArticle());
+    // Set a click handler on the close reference, calling the deleteArticle method.
+    this.closeButton.addEventListener('click', () => this.deleteArticle());
   }
 
   expandArticle() {
@@ -22,6 +26,10 @@ class Article {
       $(this.domElement).animate({height: '50px'});;
       this.expandButton.textContent = "Click to Expand";
     }
+  }
+
+  deleteArticle() {
+    $(this.domElement).fadeOut();
   }
 }
 
