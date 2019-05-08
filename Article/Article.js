2c046232a4ed668352e4f3a89e8a5551089aea48
sam-malanchuk <malanchuksam@gmail.com>
2019-05-07 20:36:33 -0400

    completed Stretch animation #2 in article.js

--- Article/Article.js
+++ Article/Article.js
@@ -7,14 +7,21 @@ class Article {
     // create a reference to the ".expandButton" class. 
     this.expandButton = this.domElement.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expandButton.textContent = "expand";
+    this.expandButton.textContent = "Click to Expand";
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener('click', () => this.expandArticle());
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-    this.domElement.classList.toggle('article-open');
+    // this.domElement.classList.toggle('article-open');
+    if(this.domElement.style.height === '' || this.domElement.style.height === '50px') {
+      $(this.domElement).animate({height: '400px'});;
+      this.expandButton.textContent = "Click to Close";
+    } else {
+      $(this.domElement).animate({height: '50px'});;
+      this.expandButton.textContent = "Click to Expand";
+    }
   }
 }
 
